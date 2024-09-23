a9a1702c81203923cc12b3551ca5266da56723a5
Lukasz Dziedziczak <wukash@gmail.com>
2024-09-23 17:48:03 +0930

    Added Characters

---
+++ Source/GAD222/ZombieStoryGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZombieStoryGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAD222_API AZombieStoryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
